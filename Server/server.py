fba91d21acd43c2c8f45b83d07a0c6fdcabcbcf0
xredocx215sevlanevets <stevenalves512@gmail.com>
2020-01-17 19:58:15 -0100

    alterando recurso  de Estudante para Item, criando endpoint get e post

--- Server/server.py
+++ Server/server.py
@@ -5,15 +5,22 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-#
+items = []
 
 
-class Estudante(Resource):
+class Item(Resource):
     def get(self, nome):
-        return {'estudante': nome}
+        for item in items:
+            if item['nome'] == nome:
+                return item
+
+    def post(self, nome):
+        item = {'nome': nome, 'preco': 12.00}
+        items.append(item)
+        return item
 
 
 # http://127.0.0.1:5000/estudante/steven
-api.add_resource(Estudante, '/estudante/<string:nome>')
+api.add_resource(Item, '/item/<string:nome>')
 
 app.run(port=5000)
